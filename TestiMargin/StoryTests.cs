5dd9576f1482f360eb81aa59dea45be4cc0d0bf8
BlaiseGratton <blaisegratton@gmail.com>
2015-02-05 15:55:56 -0600

    zero state tests

---
+++ TestiMargin/StoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.BDDfy;
+
+namespace TestiMargin
+{
+    [TestClass]
+    public class StoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
