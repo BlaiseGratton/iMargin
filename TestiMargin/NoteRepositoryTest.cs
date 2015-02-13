fe1cf31707b5580eeca444ff8f00ab985aba8c24
BlaiseGratton <blaisegratton@gmail.com>
2015-02-13 13:28:22 -0600

    setup initialize and cleanup for test methods

--- TestiMargin/NoteRepositoryTest.cs
+++ TestiMargin/NoteRepositoryTest.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using iMargin.Repository;
+using iMargin;
+using iMargin.Model;
 
 namespace TestiMargin
 {
     [TestClass]
     public class NoteRepositoryTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static NoteRepository repo;
+
+        [ClassInitialize]
+        public static void SetUp(TestContext _context)
         {
+            repo = new NoteRepository();
+            repo.Clear();
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            repo.Clear();
+        }
+
+        [TestCleanup]
+        public void ClearDatabase() 
+        {
+            repo.Clear();
+        }
+
+        [TestMethod]
+        public void TestAddToDatabase()
+        {
+            Assert.AreEqual(0, repo.GetCount());
         }
     }
 }
