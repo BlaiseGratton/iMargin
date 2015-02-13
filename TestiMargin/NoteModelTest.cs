c5c616ea0c8bb644f78293a97a72f200319c2648
BlaiseGratton <blaisegratton@gmail.com>
2015-02-13 11:20:39 -0600

    new note constructor passing

--- TestiMargin/NoteModelTest.cs
+++ TestiMargin/NoteModelTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using iMargin.Model;
 
 namespace TestiMargin
 {
@@ -7,8 +8,13 @@ namespace TestiMargin
     public class NoteModelTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreatingNoteStoresProperties()
         {
+            Note toDoNote = new Note("ToDo", "12/02/2015", "Lists", "code code code");
+            Assert.AreEqual("ToDo", toDoNote.Title);
+            Assert.AreEqual("12/02/2015", toDoNote.Date);
+            Assert.AreEqual("Lists", toDoNote.Category);
+            Assert.AreEqual("code code code", toDoNote.Content);
         }
     }
 }
